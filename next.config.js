45481a51c42b3602741a65b333602443b6db16ec
Alula Mekonen <alulamoke09@gmail.com>
2023-09-09 10:17:55 +0300

    first and last commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil"
+    });
+
+    return config;
+  },
+  images: {
+    domains: [
+      "uploadthing.com"
+    ]
+  }
+}
+
+module.exports = nextConfig
